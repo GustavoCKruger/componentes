72bb93637d853186cc29d1f33b854928258e7016
GustavoCKruger <gustavo.kruger1601@gmail.com>
2024-07-12 16:48:20 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import ExpandBox from './components/ExpandBox.vue';
+</script>
+
+<template>
+  <h1>Componentes</h1>
+  <div>
+    <ExpandBox />
+    <ExpandBox />
+    <ExpandBox />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/ExpandBox.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { computed, ref } from 'vue';
+
+const showContent = ref(false);
+const buttonText = computed(() =>
+    showContent.value ? 'Esconder' : 'Mostrar')
+</script>
+
+<template>
+    <div>
+        <button @click="showContent = !showContent">{{ buttonText }}</button>
+        <div v-if="showContent" class="expand-box">
+            Aqui vai um texto escondido
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.expand-box {
+    height: 100%;
+    padding: 20px;
+    border: 1px solid black;
+    box-shadow: 2px 2px 3px 3px gray;
+    border-radius: 10px;
+}
+</style>
